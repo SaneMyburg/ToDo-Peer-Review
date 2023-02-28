77b9c821e57f07df197ad68074791681c5099e64
SaneMyburg <sane.myburg@gmail.com>
2023-02-28 17:11:31 +0200

    Adjust the code for peer review

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import updateStatus from './check.js';
 // Display Tasks
 window.addEventListener('load', (Render.displayTasks));
 // Adding Task
-document.querySelector('.list-form').addEventListener('submit', (e) => {
-  e.preventDefault();
+document.querySelector('.list-form').addEventListener('submit', (event) => {
+  event.preventDefault();
   const userTask = document.querySelector('#add-list').value.trim();
   const tasks = Store.getToDos();
   const id = Math.floor(Math.random() * 1000);
@@ -20,30 +20,30 @@ document.querySelector('.list-form').addEventListener('submit', (e) => {
   Store.clearInput();
 });
 
-document.querySelector('.to-do-task').addEventListener('click', (e) => {
+document.querySelector('.to-do-task').addEventListener('click', (event) => {
   // Edit ToDo
-  if (e.target.classList.contains('p-element')) {
+  if (event.target.classList.contains('p-element')) {
     editTask(e.target);
   // Remove Task
-  } else if (e.target.tagName === 'I') {
-    Store.removeTask(e.target);
-    Render.deleteTask(e.target);
+  } else if (event.target.tagName === 'I') {
+    Store.removeTask(event.target);
+    Render.deleteTask(event.target);
 
     // Checked Tasks
-  } else if (e.target.className === 'check') {
+  } else if (event.target.className === 'check') {
     updateStatus(e.target);
-    if (e.target.checked) {
-      e.target.nextElementSibling.style.textDecoration = 'line-through';
+    if (event.target.checked) {
+      event.target.nextElementSibling.style.textDecoration = 'line-through';
     } else {
-      e.target.nextElementSibling.style.textDecoration = 'none';
+      event.target.nextElementSibling.style.textDecoration = 'none';
     }
   }
 });
 
 // Remove all completed
 
-document.querySelector('.clear-all').addEventListener('click', (e) => {
-  e.preventDefault();
+document.querySelector('.clear-all').addEventListener('click', (event) => {
+  event.preventDefault();
   let tasks = Store.getToDos();
   tasks = tasks.filter((todo) => todo.completed === false);
   localStorage.setItem('tasks', JSON.stringify(tasks));
